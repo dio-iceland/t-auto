88b194ecfa73195460cbcc4e12792e83efc24124
dio-iceland <dioiceland@onmail.com>
2024-12-19 20:55:50 +0200

    multiple bots and groups

--- index.js
+++ index.js
@@ -1,46 +1,102 @@
 const { Api, TelegramClient } = require('telegram');
 const { StringSession } = require('telegram/sessions');
-const input = require('input'); // npm i input
+const input = require('input');
 const cron = require('node-cron');
 require('dotenv').config();
 
-const apiId = Number(process.env.API_ID);
-const apiHash = process.env.API_HASH;
-const stringSession = new StringSession(process.env.STRING_SESSION); // fill this later with the value from session.save()
+const config = JSON.parse(process.env.CONFIG);
+const bots = config.bots;
+const groups = config.groups;
+
+let clients = [];
 
 (async () => {
-	console.log('Loading interactive example...');
-	const client = new TelegramClient(stringSession, apiId, apiHash, {
-		connectionRetries: 5,
-	});
-	// console.log(client);
-
-	await client.start({
-		phoneNumber: async () => await input.text('Please enter your number: '),
-		password: async () => await input.text('Please enter your password: '),
-		phoneCode: async () =>
-			await input.text('Please enter the code you received: '),
-		onError: (err) => console.log(err),
-	});
-	console.log('You should now be connected.');
-	// console.log(client.session.save()); // Save this string to avoid logging in again
-
-	const groupName = 'testgrouptest3';
-
-	const channel = await client.getEntity(groupName); // Replace with the channel's username or ID
-
-	async function logMessage() {
-		const time = new Date().toLocaleTimeString();
-		const result = await client.invoke(
-			new Api.messages.SendMessage({
-				peer: channel,
-				message: `Hello, time is: ${time}`,
-			})
-		);
+	console.log('Starting script...');
+	for (const bot of bots) {
+		try {
+			const stringSession = new StringSession(bot.STRING_SESSION);
+			const client = new TelegramClient(
+				stringSession,
+				Number(bot.API_ID),
+				bot.API_HASH,
+				{
+					connectionRetries: 5,
+				}
+			);
+			await client.connect();
+			clients.push(client);
+			console.log(`Number: ${bot.NR} added to clients.`);
+		} catch (error) {
+			console.error(`Failed:`, error);
+		}
 	}
 
-	// Schedule the cron job to run every minute
-	cron.schedule('* * * * *', async () => {
-		logMessage();
+	// await client.start({
+	// 	phoneNumber: async () => await input.text('Please enter your number: '),
+	// 	password: async () => await input.text('Please enter your password: '),
+	// 	phoneCode: async () =>
+	// 		await input.text('Please enter the code you received: '),
+	// 	onError: (err) => console.log(err),
+	// });
+
+	// console.log(client.session.save()); // Save this string to avoid logging in again
+
+	//Get messages from repo
+	let messages = [];
+	let activeMessage = null;
+	if (clients.length) {
+		const defaultClient = clients[0];
+		const msgRepo = await defaultClient.getEntity('myrepository9');
+		// Fetch the last 10 messages
+		messages = await defaultClient.getMessages(msgRepo, { limit: 10 });
+		messages = messages.filter((message) => message.media);
+	} else {
+		console.log('No clients connected.');
+		return;
+	}
+
+	function setActiveMessage() {
+		// If there's no active message, start with the first one
+		if (!activeMessage) {
+			activeMessage = messages[0];
+			return activeMessage;
+		}
+
+		// Find the index of the current active message
+		const currentIndex = messages.findIndex(
+			(message) => message === activeMessage
+		);
+
+		// Determine the next message index cyclically
+		const nextIndex = (currentIndex + 1) % messages.length;
+
+		// Update the active message
+		activeMessage = messages[nextIndex];
+		return activeMessage;
+	}
+
+	async function logMessages() {
+		for (const group of groups) {
+			for (const client of clients) {
+				try {
+					setActiveMessage();
+					await client.sendFile(group, {
+						file: activeMessage.media,
+						caption: activeMessage.message || '', // Include the caption if it exists
+					});
+					console.log(`Message sent to ${group}`);
+				} catch (error) {
+					console.error(
+						`Failed: client ${client.apiId} sending to ${group}`,
+						error
+					);
+				}
+			}
+		}
+	}
+
+	// Schedule the cron job
+	cron.schedule(`* * * * *`, async () => {
+		logMessages();
 	});
 })();
